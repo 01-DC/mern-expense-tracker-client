b14684b9812acf02e5c2d56e6355812b160cd0f2
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-12-17 14:46:34 +0530

    update baseUrl for deployed api

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import axios from "axios"
 import { BrowserRouter } from "react-router-dom"
 import { ContextProvider } from "./contexts/ContextProvider"
 
 import App from "./App"
 import "./index.css"
+
+axios.defaults.baseURL = "https://mern-expense-tracker-api.onrender.com"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,4 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		proxy: {
-			"/api/v1": {
-				target: "http://localhost:8080",
-			},
-		},
-	},
 })
